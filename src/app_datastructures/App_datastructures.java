8b16044ae0a6ef9d18c3bf302f38f2586e7301b5
hraisinz <hraisinz@gmail.com>
2019-08-21 18:27:37 +0530

    Stack Implementation

---
+++ src/app_datastructures/App_datastructures.java
@@ -0,0 +1,17 @@
+package app_datastructures;
+
+import com.raisinz.model.Stack;
+import service.StackClient;
+
+/**
+ *
+ * @author hardikpatel
+ */
+public class App_datastructures {
+
+    public static void main(String[] args) {
+        Stack s1 = new Stack(10);
+        StackClient.getStack(s1);
+    }
+    
+}
